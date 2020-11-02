2cb6e84ac4154a7241362e3654050d6a2b3985c7
Guilherme Guimarães <guima_dev@hotmail.com>
2020-11-02 20:27:21 -0300

    Implementacao da API em Node do desafio Deloitte com geracao de dados aleatorios

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,24 +3,72 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/dashboard/ambientes', (request, response) => {
-    const data = {
-        sandbox: 18,
-        produção: 25,
-        stage: 4
-    }
+    const labels = ['Sandbox', 'Produção', 'Stage'];
+    const data = labels.map(value => {
+        return {
+            label: value,
+            quantidade: generateData()
+        }
+    })
 
     return response.json(data)
 })
 
 routes.get('/dashboard/consumo', (request, response) => {
-    const data = [
-        {
-            label: 'Ec Varejo',
-
+    const labels = ['Ec. Varejo', 'Portador', 'Credenciador', 'Emissor', 'Comunidade']
+    const data = labels.map(value => {
+        return {
+            label: value,
+            portador: generateData(),
+            token: generateData(),
+            bins: generateData(),
+            pricing: generateData(),
+            facilitador: generateData(),
+            histTrans: generateData(),
         }
-    ]
+    })
 
     return response.json(data)
 })
+
+routes.get('/dashboard/planejamento', (request, response) => {
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Novembro', 'Dezembro']
+    const data = months.map(value => {
+        return {
+            label: value,
+            realizado: generateData(),
+            planejado: generateData()
+        }
+    })
+
+    return response.json(data)
+})
+
+routes.get('/dashboard/doughnut', (request, response) => {
+    const labels = ['Ec. Varejo', 'Portador', 'Credenciador', 'Emissor', 'Comunidade']
+    const data = labels.map(value => {
+        return {
+            label: value,
+            quantidade: generateData()
+        }
+    })
+
+    response.json(data)
+})
+
+routes.get('/dashboard/faturamento', (request, response) => {
+    return response.json(generateData() * 921)
+})
+
+routes.get('/dashboard/tempoMedioDeIntegracao', (request, response) => {
+    return response.json(generateData())
+})
+
+routes.get('/dashboard/numeroDeAplicacoes', (request, response) => {
+    return response.json(generateData() * 10)
+})
+
+const generateData = () => Number((Math.random() * 10).toFixed(0))
+
 
 module.exports = routes
